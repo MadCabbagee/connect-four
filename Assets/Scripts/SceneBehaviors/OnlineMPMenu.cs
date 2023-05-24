cd6592e5e37e1efc7a7f2e3347ee204886b7e9d3
MadCabbage <justin.t.mclean@gmail.com>
2023-05-24 09:16:28 -0600

    create scene for online MP menu

---
+++ Assets/Scripts/SceneBehaviors/OnlineMPMenu.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using SceneBehaviors;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnlineMPMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject listItemPrefab;
+    [SerializeField] private Transform listContentContainer;
+    [SerializeField] private TMP_InputField passwordField;
+    [SerializeField] private Button joinButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*asswordField.gameObject.SetActive(false);
+        joinButton.gameObject.SetActive(false);*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BackButton_OnClick()
+    {
+        MainMenu.SwitchScene("MultiplayerMenuScene");
+    }
+
+    public void RefreshButton_OnClick()
+    {
+        List<string> onlineGames = GetOnlineGamesList();
+        
+        // todo: unselect selected game
+        passwordField.gameObject.SetActive(false);
+        joinButton.gameObject.SetActive(false);
+    }
+
+    private void PopulateGameList(List<string> games)
+    {
+        foreach (string game in games)
+        {
+            GameObject listItem = Instantiate(listItemPrefab, listContentContainer);
+            listItem.GetComponentInChildren<Text>().text = game;
+        }
+    }
+
+    private List<string> GetOnlineGamesList()
+    {
+        // todo: refresh the list by requesting the information from the server again via websocket and repopulating the list
+        throw new NotImplementedException();
+    }
+    
+    public void HostButton_OnClick()
+    {
+        MainMenu.SwitchScene("HostOnlineGameScene");
+    } 
+
+    public void JoinButton_OnClick()
+    {
+        // todo: Send join request over websocket
+    }
+
+    public void OnListItemSelected()
+    {
+        passwordField.gameObject.SetActive(true);
+        joinButton.gameObject.SetActive(true);
+    }
+}
