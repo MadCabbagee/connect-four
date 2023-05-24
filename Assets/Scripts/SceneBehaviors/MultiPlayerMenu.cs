0317c05ba377afb9ffead7e0830a822a36fad01d
MadCabbage <justin.t.mclean@gmail.com>
2023-05-24 09:15:47 -0600

    handle back button click, handle local button click, handle online button click

---
+++ Assets/Scripts/SceneBehaviors/MultiPlayerMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace SceneBehaviors
+{
+    public class MultiPlayerMenu : MonoBehaviour
+    {
+        public void BackButton_OnClick()
+        {
+            MainMenu.SwitchScene("MainMenuScene");
+        }
+
+        public void LocalButton_OnClick()
+        {
+            MainMenu.SwitchScene("LocalMultiplayerMenuScene");
+            MainMenu.GameMode = GameMode.LocalMultiplayer;
+        }
+
+        public void OnlineButton_OnClick()
+        {
+            MainMenu.SwitchScene("OnlineMultiplayerMenuScene");
+            MainMenu.GameMode = GameMode.OnlineMultiplayer;
+        }
+    }
+}
