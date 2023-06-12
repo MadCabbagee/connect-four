dcd63f908ddafb68e3d880ec77cc1bfadfcdda28
MadCabbage <justin.t.mclean@gmail.com>
2023-06-11 18:57:35 -0600

    create PlayerController skeleton

---
+++ Assets/Scripts/GameLogic/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿namespace GameLogic
+{
+    public class PlayerController
+    {
+        protected Referee referee;
+
+        public void Initialize(Referee referee)
+        {
+            this.referee = referee;
+        }
+
+        public void OnPlayerInput(int column)
+        {
+            referee.PlayMove(column);
+        }
+    }
+}
