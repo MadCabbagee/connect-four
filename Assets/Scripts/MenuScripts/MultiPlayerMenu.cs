5018fef2f816be1b7fa7892970ce45c02c41743e
MadCabbage <justin.t.mclean@gmail.com>
2023-06-12 15:24:43 -0600

    set gamemode to none on backbutton click. switch scene to PlayerSetupScene on localbutton and onlinebutton click

--- Assets/Scripts/MenuScripts/MultiPlayerMenu.cs
+++ Assets/Scripts/MenuScripts/MultiPlayerMenu.cs
@@ -1,24 +1,25 @@
 ﻿using UnityEngine;
 
-namespace UIBehaviors
+namespace MenuScripts
 {
     public class MultiPlayerMenu : MonoBehaviour
     {
         public void BackButton_OnClick()
         {
+            MainMenu.GameMode = GameMode.None;
             MainMenu.SwitchScene("MainMenuScene");
         }
 
         public void LocalButton_OnClick()
         {
-            MainMenu.SwitchScene("LocalMultiplayerMenuScene");
             MainMenu.GameMode = GameMode.LocalMultiplayer;
+            MainMenu.SwitchScene("PlayerSetupScene");
         }
 
         public void OnlineButton_OnClick()
         {
-            MainMenu.SwitchScene("OnlineMultiplayerMenuScene");
             MainMenu.GameMode = GameMode.OnlineMultiplayer;
+            MainMenu.SwitchScene("PlayerSetupScene");
         }
     }
 }
