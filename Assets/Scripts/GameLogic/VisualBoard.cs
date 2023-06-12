9bcc3df20e6a144363ee9381aa0e494ef5e219f0
MadCabbage <justin.t.mclean@gmail.com>
2023-06-11 18:57:50 -0600

    create VisualBoard skeleton

---
+++ Assets/Scripts/GameLogic/VisualBoard.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace GameLogic
+{
+    public class VisualBoard : MonoBehaviour
+    {
+        // Subscribe to changes in the abstract board and update the visual representation
+        // ...
+        public void Initialize(Referee referee)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
