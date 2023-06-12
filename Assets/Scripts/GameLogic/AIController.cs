1d0a468b03bc8fcd813198b6b302eb02d58e2a7d
MadCabbage <justin.t.mclean@gmail.com>
2023-06-11 18:55:30 -0600

    create AIController skeleton

---
+++ Assets/Scripts/GameLogic/AIController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace GameLogic
+{
+    public class AIController : MonoBehaviour
+    {
+        protected Referee referee;
+
+        public void Initialize(Referee referee)
+        {
+            this.referee = referee;
+        }
+
+        public void MakeMove()
+        {
+            // Calculate the next move for the AI
+            int column = CalculateNextMove();
+            referee.PlayMove(column);
+        }
+
+        private int CalculateNextMove()
+        {
+            // Perform AI logic to determine the next move
+            // ...
+            return 0;
+        }
+    }
+}
