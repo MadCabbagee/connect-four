5befa45f483536271df34b3543279dc3055a28f7
MadCabbage <justin.t.mclean@gmail.com>
2023-06-11 18:55:40 -0600

    create AIReferee skeleton

---
+++ Assets/Scripts/GameLogic/AIReferee.cs
@@ -0,0 +1,26 @@
+﻿namespace GameLogic
+{
+    public class AIReferee : Referee
+    {
+        private AIController aiController;
+        private int currentPlayer = 1;
+
+        public AIReferee(AIController aiController)
+        {
+            this.aiController = aiController;
+        }
+
+        public override void PlayMove(int column)
+        {
+            // Perform game logic for playing against AI
+            // ...
+            // Update the game board based on the move
+            // ...
+            // Switch the player
+            currentPlayer = (currentPlayer == 1) ? 2 : 1;
+
+            // Let the AI controller make its move
+            aiController.MakeMove();
+        }
+    }
+}
