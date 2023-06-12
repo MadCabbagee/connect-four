c86794453667bfa7baea3def2a640ea4c8d4c92e
MadCabbage <justin.t.mclean@gmail.com>
2023-06-11 18:57:06 -0600

    create MultiplayerController skeleton

---
+++ Assets/Scripts/GameLogic/MultiplayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace GameLogic
+{
+    public class MultiplayerController : MonoBehaviour
+    {
+        protected Referee referee;
+
+        public void Initialize(Referee referee)
+        {
+            this.referee = referee;
+        }
+
+        public void OnOpponentMoveReceived(int column)
+        {
+            referee.PlayMove(column);
+        }
+    }
+}
