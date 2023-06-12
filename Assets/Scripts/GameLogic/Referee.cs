4de67dade19f194f323ef92db3ee98d3be354763
MadCabbage <justin.t.mclean@gmail.com>
2023-06-11 18:57:40 -0600

    create Referee skeleton

---
+++ Assets/Scripts/GameLogic/Referee.cs
@@ -0,0 +1,16 @@
+﻿namespace GameLogic
+{
+    public abstract class Referee
+    {
+        protected GameBoard gameBoard;
+
+        public void StartGame()
+        {
+            // Initialize the game board
+            gameBoard = new GameBoard();
+            // ...
+        }
+
+        public abstract void PlayMove(int column);
+    }
+}
