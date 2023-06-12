09d088ddbe6042f00d4dbc01baabe8adbcdef2cf
MadCabbage <justin.t.mclean@gmail.com>
2023-06-11 18:56:56 -0600

    create LocalMultiplayerReferee skeleton

---
+++ Assets/Scripts/GameLogic/LocalMultiplayerReferee.cs
@@ -0,0 +1,17 @@
+﻿namespace GameLogic
+{
+    public class LocalMultiplayerReferee : Referee
+    {
+        private int currentPlayer = 1;
+
+        public override void PlayMove(int column)
+        {
+            // Perform game logic for local multiplayer mode
+            // ...
+            // Update the game board based on the move
+            // ...
+            // Switch the player
+            currentPlayer = (currentPlayer == 1) ? 2 : 1;
+        }
+    }
+}
