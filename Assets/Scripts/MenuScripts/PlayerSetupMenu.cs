edf18840baa4bde0d4cf727e8c805d0d4df217be
MadCabbage <justin.t.mclean@gmail.com>
2023-06-12 15:22:01 -0600

    renamed from 'LocalMultiplayer*'

---
+++ Assets/Scripts/MenuScripts/PlayerSetupMenu.cs
@@ -0,0 +1,75 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MenuScripts
+{
+    public class PlayerSetupMenu : MonoBehaviour
+    {
+        [SerializeField] private GameObject playerOneSettings;
+        [SerializeField] private GameObject playerTwoSettings;
+
+        private string _lastMenu;
+        private void OnEnable()
+        {
+            
+            switch (MainMenu.GameMode)
+            {
+                default:
+                    ShowMainMenu();
+                    break;
+                
+                case GameMode.None:
+                    ShowMainMenu();
+                    break;
+                
+                case GameMode.SinglePlayer:
+                    _lastMenu = "MainMenuScene";
+                    ShowSettings(true);
+                    break;
+                
+                case GameMode.LocalMultiplayer:
+                    _lastMenu = "MultiPlayerMenuScene";
+                    ShowSettings(false);
+                    break;
+                
+                case GameMode.OnlineMultiplayer:
+                    _lastMenu = "MultiPlayerMenuScene";
+                    ShowSettings(true);
+                    break;
+            }
+        }
+
+        private void ShowSettings(bool showOnlyOne)
+        {
+            if (showOnlyOne)
+            {
+                playerOneSettings.SetActive(true);
+                playerTwoSettings.SetActive(false);
+            }
+            else
+            {
+                playerOneSettings.SetActive(true);
+                playerTwoSettings.SetActive(true);
+            }
+        }
+
+        private static void ShowMainMenu()
+        {
+            MainMenu.SwitchScene("MainMenuScene");
+        }
+
+        public void BackButton_OnClick()
+        {
+            MainMenu.SwitchScene(_lastMenu);
+            MainMenu.GameMode = GameMode.None;
+        }
+
+        public void PlayButton_OnClick()
+        {
+            MainMenu.SwitchScene("GameScene");
+        }
+    }
+}
+
